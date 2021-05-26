43d7e97033b80f4ff9104540051471dfa8d06625
Dan Gower <gower.daniel.j@gmail.com>
2021-05-26 00:12:37 -0500

    finished excercises 4.7, which is in the lsn3classes package

--- src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
+++ src/org/launchcode/java/demos/lsn3classes1/SchoolPractice.java
@@ -1,7 +1,51 @@
 package org.launchcode.java.demos.lsn3classes1;
 
+import java.util.ArrayList;
+
 public class SchoolPractice {
     public static void main(String[] args) {
         // Instantiate your Student class for part 2 here!
+        Student student1 = new Student();
+        student1.setName("Dan");
+        student1.setNumberOfCredits(1);
+        student1.setGpa(4.0);
+    }
+
+    public class Course {
+        private String title;
+        private String professor;
+        private ArrayList<Student> enrolledStudents;
+    }
+
+    protected class Teacher {
+        private String firstName;
+        private String lastName;
+        private String subject;
+        private Integer yearsTeaching;
+
+        protected void setFirstName(String firstName){
+            this.firstName = firstName;
+        }
+        protected void setLastName(String lastName){
+            this.lastName = lastName;
+        }
+        protected void setSubject(String subject){
+            this.subject = subject;
+        }
+        protected void setYearsTeaching(Integer yearsTeaching){
+            this.yearsTeaching = yearsTeaching;
+        }
+        protected String getFirstName(){
+            return firstName;
+        }
+        protected String getLastName(){
+            return lastName;
+        }
+        protected String getSubject(){
+            return subject;
+        }
+        protected Integer getYearsTeaching(){
+            return yearsTeaching;
+        }
     }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -10,4 +10,29 @@ public class Student {
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    protected void setName(String name){
+        this.name = name;
+    }
+    protected void setStudentId(int studentId){
+        this.studentId = studentId;
+    }
+    protected void setNumberOfCredits(int numberOfCredits){
+        this.numberOfCredits = numberOfCredits;
+    }
+    protected void setGpa(double gpa){
+        this.gpa = gpa;
+    }
+    protected String getName(){
+        return name;
+    }
+    protected int getStudentId(){
+        return studentId;
+    }
+    protected int getNumberOfCredits(){
+        return numberOfCredits;
+    }
+    protected double getGpa(){
+        return gpa;
+    }
+
 }
